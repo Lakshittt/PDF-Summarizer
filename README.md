24f7d21010ed7c38c3849afddb03fc5f2604258d
Lakshit <laxitjain1212@gmail.com>
2024-05-11 12:33:20 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# 📄 PDF Summarizer
+
+## Description
+This PDF Summarizer project aims to address the need for efficiently
+summarizing PDF documents, making it easier for users to extract key
+information from lengthy texts. The primary objectives of the project are to
+develop a user-friendly interface for uploading PDF files, selecting specific
+pages or ranges for summarization, generating summaries using advanced
+natural language processing techniques, and enabling question-answering
+functionality based on the content of the documents.
+
+The system is built with the aim of summarising a PDF file uploaded by the user. This system was created using Streamlit, LangChain, and OpenAI frameworks.
+
+## Preview
+To be able to see a preview of this system, please see the following link: https://pdf-summarizerr.streamlit.app/,  or check out the preview below.
+## Technologies used
+
+Python
+
+Streamlit
+
+PyPDF2
+
+Langchain
+
+Hugging Face Transformers
+
+OpenAI API
+## How to Use
+In the lines of program code below, change ``OPENAI_API_KEY`` to match your OPENAI API KEY. 
+
+```bash
+openai_api_key = os.getenv("OPENAI_API_KEY")
+```
+Clone the repository:
+```bash
+git clone https://github.com/your-username/pdf-summarizer.git
+
+```
+Navigate to the project directory:
+```bash
+cd pdf-summarizer
+```
+Install dependencies:
+```bash
+pip install -r requirements.txt
+```
+Run the application:
+```bash
+streamlit run app.py
+```
+
+
+
---
+++ app.py
@@ -0,0 +1,87 @@
+# Importing necessary libraries and modules
+import streamlit as st  # Streamlit library for creating the user interface
+from langchain.embeddings.openai import OpenAIEmbeddings  # Module for embeddings using OpenAI language models
+import tempfile  # Module for handling temporary files
+import time  # Module for time-related operations
+from langchain import OpenAI, PromptTemplate, LLMChain  # Classes from Langchain library
+from langchain.text_splitter import CharacterTextSplitter  # Class for splitting text into smaller chunks
+from langchain.document_loaders import PyPDFLoader  # Class for loading and splitting PDF documents
+from langchain.chains.summarize import load_summarize_chain  # Function for loading summarization chain
+from langchain.docstore.document import Document  # Class representing a document
+from langchain.vectorstores import FAISS  # Class for vector indexing and similarity search
+from langchain.chains.question_answering import load_qa_chain  # Function for loading question-answering chain
+
+# Initializing OpenAI language model
+
+openapi_key = "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"
+llm = OpenAI(openai_api_key=openapi_key, temperature=0)
+
+# Initializing text splitter
+text_splitter = CharacterTextSplitter(
+    separator="\n",
+    chunk_size=800,
+    chunk_overlap=200,
+    length_function=len,
+)
+
+# Streamlit application title and author
+st.title("📄PDF Summarizer")
+
+# File uploader to upload PDF files
+pdf_file = st.file_uploader("Choose a PDF file", type="pdf")
+
+# If a PDF file is uploaded
+if pdf_file is not None:
+    # Temporary file creation to store the uploaded PDF
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        tmp_file.write(pdf_file.read())
+        pdf_path = tmp_file.name
+        # Loading and splitting PDF pages
+        loader = PyPDFLoader(pdf_path)
+        pages = loader.load_and_split()
+
+        # User input for page selection
+        page_selection = st.radio("Page selection", ["Single page", "Page range", "Overall Summary"])
+
+        # If single page selection is chosen
+        if page_selection == "Single page":
+            page_number = st.number_input("Enter page number", min_value=1, max_value=len(pages), value=1, step=1)
+            view = pages[page_number - 1]
+            texts = text_splitter.split_text(view.page_content)
+            docs = [Document(page_content=t) for t in texts]
+            chain = load_summarize_chain(llm, chain_type="map_reduce")
+            summaries = chain.run(docs)
+
+            st.subheader("Summary")
+            st.write(summaries)
+
+        # If page range selection is chosen
+        elif page_selection == "Page range":
+            start_page = st.number_input("Enter start page", min_value=1, max_value=len(pages), value=1, step=1)
+            end_page = st.number_input("Enter end page", min_value=start_page, max_value=len(pages), value=start_page,
+                                       step=1)
+
+            texts = []
+            for page_number in range(start_page, end_page + 1):
+                view = pages[page_number - 1]
+                page_texts = text_splitter.split_text(view.page_content)
+                texts.extend(page_texts)
+            docs = [Document(page_content=t) for t in texts]
+            chain = load_summarize_chain(llm, chain_type="map_reduce")
+            summaries = chain.run(docs)
+            st.subheader("Summary")
+            st.write(summaries)
+
+        # If overall summary selection is chosen
+        elif page_selection == "Overall Summary":
+            combined_content = ''.join([p.page_content for p in pages])  # Concatenating entire page content
+            texts = text_splitter.split_text(combined_content)
+            docs = [Document(page_content=t) for t in texts]
+            chain = load_summarize_chain(llm, chain_type="map_reduce")
+            summaries = chain.run(docs)
+            st.subheader("Summary")
+            st.write(summaries)
+
+else:
+    time.sleep(30)
+    st.warning("No PDF file uploaded", "🚨")  # Warning if no PDF file is uploaded
